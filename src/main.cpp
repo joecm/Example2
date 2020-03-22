b8d432316c113ccc5515c337bd1532e5a412573e
José Antonio Costa de Moya <joseantonio.costa250@gmail.com>
2020-03-22 11:23:33 +0100

    Inicial COmmit

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+/*********************************************************************
+ * Módulo 2. Curso de Experto en Desarrollo de Videojuegos
+ * Autor: Carlos González Morcillo     Carlos.Gonzalez@uclm.es
+ *
+ * You can redistribute and/or modify this file under the terms of the
+ * GNU General Public License ad published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * and later version. See <http://www.gnu.org/licenses/>.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.  
+ *********************************************************************/
+
+#include "ExampleApplication.h"
+
+class SimpleExample : public ExampleApplication {
+  public : void createScene() {   
+    Ogre::Entity *ent = mSceneMgr->createEntity("Sinbad", "Sinbad.mesh");
+    mSceneMgr->setAmbientLight(Ogre::ColourValue(1, 1, 1));
+    Ogre::SceneNode* node = mSceneMgr->createSceneNode("Node1");
+    mSceneMgr->getRootSceneNode()->addChild(node);
+    node->attachObject(ent);
+  }
+};
+
+int main(void) {
+  SimpleExample example;
+  example.go();
+  
+  return 0;
+}
